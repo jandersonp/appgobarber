8f71004f988bd46dfc4fe5610b86530384410a67
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-05-26 00:24:49 -0300

    Input & Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,9 +3,10 @@ import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
   width: 100%;
-  height: 60px;
+  height: 50px;
   background: #ff9000;
   border-radius: 10px;
+  margin-top: 8px;
 
   align-items: center;
   justify-content: center;
@@ -14,5 +15,5 @@ export const Container = styled(RectButton)`
 export const ButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
   color: #312e38;
-  font-size: 18px;
+  font-size: 16px;
 `;
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 export const Container = styled.View`
   width: 100%;
-  height: 60px;
+  height: 50px;
   background: #232129;
   padding: 0 16px;
   border-radius: 10px;
   margin-bottom: 8px;
+
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const TextInput = styled.TextInput`
@@ -14,4 +18,8 @@ export const TextInput = styled.TextInput`
   color: #f4ede8;
   font-size: 16px;
   font-family: 'RobotoSlab-Regular';
+`;
+
+export const Icon = styled(FeatherIcon)`
+  margin-right: 8px;
 `;
